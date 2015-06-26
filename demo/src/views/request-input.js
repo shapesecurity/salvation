643dc2706fc01d45d49887f27cb7a4b84df60f57
Ben Vinegar <ben@benv.ca>
2015-06-26 11:55:34 -0700

    Better homepage styles

--- demo/src/views/request-input.js
+++ demo/src/views/request-input.js
@@ -7,64 +7,51 @@ export default function() {
   <head>
     <meta charset="utf-8">
     <title>CSP Header Inspector and Validator</title>
-    <style>
-      body {
-        zoom: 200%;
-        padding: 2em;
-      }
-      form p {
-        display: flex;
-      }
-      form input {
-        flex: 1;
-        min-width: 6em;
-      }
-      form button {
-        width: 3em;
-      }
-      form button, form select, form input {
-        margin: 0 0.2em;
-        font-size: 1em;
-      }
-      .or {
-        text-align: center;
-        font-size: x-large;
-      }
-    </style>
     <link href="/css/bootstrap.css" type="text/css" rel="stylesheet"/>
     <script src="/js/jquery.js"></script>
     <script src="/js/validator.js"></script>
   </head>
   <body>
-    <form action="/fetchHeader">
-      <div class="row">
-        <div class="col-md-4">
-          <input type="url" name="url" placeholder="https://..." class="form-control"/>
+    <div class="container">
+      <h1 class="page-header">Content Security Policy (CSP) Validator</h1>
+
+      <h2>Validate URL</h2>
+      <p>Validate CSP headers as served from the given URL.</p>
+      <form action="/fetchHeader" class="well">
+        <div class="row">
+          <div class="col-md-10">
+            <input type="url" name="url" placeholder="https://..." class="form-control"/>
+          </div>
+          <div class="col-md-2">
+            <button class="btn">Go</button>
+          </div>
         </div>
-        <div class="col-md-4">
-          <button class="form-control">Go</button>
+      </form>
+
+      <h2>Validate CSP String</h2>
+      <p>Validate a raw CSP header/value string.</p>
+      <form action="/directHeader" class="well">
+        <div class="row">
+          <div class="col-md-3">
+            <select name="headerName[]" class="form-control">
+              <option>Content-Security-Policy:</option>
+              <option>Content-Security-Policy-Report-Only:</option>
+            </select>
+          </div>
+          <div class="col-md-7">
+            <input type="text" name="headerValue[]" width="120" class="form-control"/>
+          </div>
+          <div class="col-md-2">
+            <button class="btn">Go</button>
+          </div>
         </div>
+      </form>
+      <!-- TODO: make this pretty -->
+      <div id="output">
       </div>
-    </form>
-    <p class="or"> OR </p>
-    <form action="/directHeader">
-      <div class="row">
-        <div class="col-md-4">
-          <select name="headerName[]" class="form-control">
-            <option>Content-Security-Policy:</option>
-            <option>Content-Security-Policy-Report-Only:</option>
-          </select>
-        </div>
-        <div class="col-md-4">
-          <input type="text" name="headerValue[]" width="120" class="form-control"/>
-        </div>
-        <div class="col-md-2">
-          <button class="form-control">Go</button>
-        </div>
-      </div>
-    </form>
-    <!-- TODO: make this pretty -->
-    <div id="output">
+
+      <hr>
+      <p>CSP Validator was built by Sergey Shekyan, Michael Ficarra, Dawson Botsford, Ben Vinegar, and the fine folks at <a href="http://shapesecurity.com">Shape Security</a>.</p>
     </div>
   </body>
 </html>`;

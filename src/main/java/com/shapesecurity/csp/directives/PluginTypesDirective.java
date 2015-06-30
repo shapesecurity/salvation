888ba01922f6c78d741ee8839136d327a95315d1
Sergey Shekyan <shekyan@gmail.com>
2015-06-30 12:56:35 -0700

    Fixes #7: allowsPlugin() works incorrectly
    Implemented matcher for MediaType, added tests..

--- src/main/java/com/shapesecurity/csp/directives/PluginTypesDirective.java
+++ src/main/java/com/shapesecurity/csp/directives/PluginTypesDirective.java
@@ -1,15 +1,21 @@
 package com.shapesecurity.csp.directives;
 
 import com.shapesecurity.csp.directiveValues.MediaType;
+import com.shapesecurity.csp.interfaces.MatchesMediaType;
 
 import javax.annotation.Nonnull;
 import java.util.List;
 
-public class PluginTypesDirective extends MediaTypeListDirective {
+public class PluginTypesDirective extends MediaTypeListDirective implements MatchesMediaType {
     @Nonnull
     private static final String name = "plugin-types";
 
     public PluginTypesDirective(@Nonnull List<MediaType> mediaTypes) {
         super(PluginTypesDirective.name, mediaTypes);
+    }
+
+    public boolean matchesMediaType(@Nonnull MediaType mediaType) {
+        return this.values()
+                .anyMatch(x -> x.matchesMediaType(mediaType));
     }
 }

c9a732c726bb1582901600ce213c38934a0330ed
Ross Troha <r.troha@f5.com>
2024-01-22 10:21:32 -0700

    expose functions on window so they're individually callable

---
+++ src/main/java/com/shapesecurity/salvation2/Salvation2.java
@@ -0,0 +1,34 @@
+package com.shapesecurity.salvation2;
+
+import org.teavm.jso.JSBody;
+
+public class Salvation2 {
+	public static void main(String[] args) {
+		initParseList();
+		initParseSingle();
+	}
+
+	public static void parseSerializedCSPList(String policyText) {
+		Policy.parseSerializedCSPList(policyText, (severity, message, policyIndex, directiveIndex, valueIndex) -> {
+			System.err.println(severity.name() + " at directive " + directiveIndex + (valueIndex == -1 ? "" : " at value " + valueIndex) + ": " + message);
+		});
+	}
+
+	public static void parseSerializedCSP(String policyText) {
+		Policy.parseSerializedCSP(policyText, (severity, message, directiveIndex, valueIndex) -> {
+			System.err.println(severity.name() + " at directive " + directiveIndex + (valueIndex == -1 ? "" : " at value " + valueIndex) + ": " + message);
+		});
+	}
+	@JSBody(params={}, script=
+		"window.parseSerializedCSPList = (policyText) => {\n" +
+		"return javaMethods.get('com.shapesecurity.salvation2.Salvation2.parseSerializedCSPList(Ljava/lang/String;)V').invoke(policyText)\n" +
+		"}")
+	static native void initParseList();
+
+	@JSBody(params={}, script=
+		"window.parseSerializedCSP = (policyText) => {\n" +
+			"return javaMethods.get('com.shapesecurity.salvation2.Salvation2.parseSerializedCSP(Ljava/lang/String;)V').invoke(policyText)\n" +
+			"}")
+	static native void initParseSingle();
+}
+

6c268cf7dbc3a55a1a0685ae98b0c8c57c033fe6
Ross Troha <r.troha@f5.com>
2024-07-10 12:00:13 -0600

    fix indentation

--- src/main/java/com/shapesecurity/salvation2/Main.java
+++ src/main/java/com/shapesecurity/salvation2/Main.java
@@ -1,17 +1,17 @@
 package com.shapesecurity.salvation2;
 
 public class Main {
-		public static void main(String[] args) {
-			if (args.length == 0) {
-				throw new IllegalArgumentException("Please provide a comma separated list of policies.");
-			}
-
-			String policyText = String.join(",", args);
-
-			PolicyList policies = Policy.parseSerializedCSPList(policyText, (severity, message, policyIndex, directiveIndex, valueIndex) -> {
-				System.err.println(severity.name() + " at directive " + directiveIndex + (valueIndex == -1 ? "" : " at value " + valueIndex) + ": " + message);
-			});
-
-			System.out.println("Finished validating policies.");
+	public static void main(String[] args) {
+		if (args.length == 0) {
+			throw new IllegalArgumentException("Please provide a comma separated list of policies.");
 		}
+
+		String policyText = String.join(",", args);
+
+		PolicyList policies = Policy.parseSerializedCSPList(policyText, (severity, message, policyIndex, directiveIndex, valueIndex) -> {
+			System.err.println(severity.name() + " at directive " + directiveIndex + (valueIndex == -1 ? "" : " at value " + valueIndex) + ": " + message);
+		});
+
+		System.out.println("Finished validating policies.");
+	}
 }

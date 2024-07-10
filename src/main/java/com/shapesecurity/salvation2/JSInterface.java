8839434cc74c3435be6b6fc3e0fdf1cc82560fdc
Ross Troha <r.troha@f5.com>
2024-07-10 12:00:14 -0600

    applying comment suggestions

--- src/main/java/com/shapesecurity/salvation2/JSInterface.java
+++ src/main/java/com/shapesecurity/salvation2/JSInterface.java
@@ -2,7 +2,7 @@ package com.shapesecurity.salvation2;
 
 import org.teavm.jso.JSBody;
 
-public class Salvation2 {
+public class JSInterface {
 	public static void main(String[] args) {
 		initParseList();
 		initParseSingle();
@@ -39,13 +39,13 @@ public class Salvation2 {
 
 	@JSBody(params = {}, script =
 		"(window || globalThis).getErrorsForSerializedCSPList = (policyText) => {\n" +
-		"return javaMethods.get('com.shapesecurity.salvation2.Salvation2.getErrorsForSerializedCSPList(Ljava/lang/String;)Ljava/lang/String;').invoke(policyText)\n" +
+		"return javaMethods.get('com.shapesecurity.salvation2.JSInterface.getErrorsForSerializedCSPList(Ljava/lang/String;)Ljava/lang/String;').invoke(policyText)\n" +
 		"}")
 	static native void initParseList();
 
 	@JSBody(params = {}, script =
 		"(window || globalThis).getErrorsForSerializedCSP = (policyText) => {\n" +
-			"return javaMethods.get('com.shapesecurity.salvation2.Salvation2.getErrorsForSerializedCSP(Ljava/lang/String;)Ljava/lang/String;').invoke(policyText)\n" +
+			"return javaMethods.get('com.shapesecurity.salvation2.JSInterface.getErrorsForSerializedCSP(Ljava/lang/String;)Ljava/lang/String;').invoke(policyText)\n" +
 			"}")
 	static native void initParseSingle();
 }

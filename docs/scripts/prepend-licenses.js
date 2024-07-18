d101fcc8fd4d8c26e0b5469f2681ad3fe2f70495
disnet <tim.disney@gmail.com>
2024-07-18 19:52:43 +0000

    Deploying to gh-pages from @ shapesecurity/salvation@617b96f3b25cfb76e6ce651d3b4c7c8ccb2176bc 🚀

--- docs/scripts/prepend-licenses.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-
-const mainJsPath = __dirname + '/../public/main.js';
-const mainJsContent = fs.readFileSync(mainJsPath).toString();
-
-fs.writeFileSync(mainJsPath, '// Licenses available at https://cspvalidator.org/licenses.json\n' + mainJsContent);
